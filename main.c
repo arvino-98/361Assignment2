3fceda66c0582bcc255144fff0055e015f99fdbe
Arvin Aya-ay <arvino@udel.edu>
2018-09-25 10:55:37 -0400

    starting code on signal handling

--- main.c
+++ main.c
@@ -8,11 +8,16 @@ void sig_handler(int signal);
 int main( int argc, char **argv, char **envp )
 {
   /* put signal set up stuff here */
-  
+  if (signal(SIGINT, SIG_IGN) != SIG_IGN){
+    signal(SIGINT, sig_handler);
+  }
+
   return sh(argc, argv, envp);
 }
 
-void sig_handler(int signal)
+void sig_handler(int sig)
 {
   /* define your signal handler */
+  signal(sig, SIG_IGN);
+  printf("\n Cannot be terminated with Ctrl+C \n");
 }
